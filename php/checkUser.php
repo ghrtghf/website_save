141d099c032bc53270b54b3d8351c543c54382f4
gastarbaiter <idinahui@gmail.com>
2024-03-25 11:46:47 +0500

    last2

--- php/checkUser.php
+++ php/checkUser.php
@@ -1,17 +1,10 @@
 <?php
 require_once __DIR__ . '/healpers.php';
 
-$email = $_POST['email'] ?? null;
-$password = $_POST['password'] ?? null;
+$message = json_decode(file_get_contents('php://input'));
 
-addOldValues('email', $email);
-
-if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-	addValidationError('email', 'Validation Error'); 
-	addValidationError('email', 'The email address is invalid.');
-	redirect('/login.php');
-}
+$email = $message -> email ?? null;
+$password = $message -> password ?? null;
 
 $user = findUser($email);
 
@@ -22,8 +15,12 @@ if (!password_verify($password, $user['password'])) {
 	addValidationError('password', 'Incorrect password');
 }
 
-$_SESSION['user']['id'] = $user['id'];
-$_SESSION['user']['role'] = $user['role'];
+if(!isset($_SESSION['validation'])){
+	$message = 2;
+}else{
+	$_SESSION['user']['id'] = $user['id'];
+	$_SESSION['user']['role'] = $user['role'];
+	$message = 1;
+};
 
-$message[] = json_decode(file_get_contents('php://input'));
 echo json_encode($message);
